0dca266d8d92e739dddd973eede559f948c86f4a
Ildar <tag.ildar@gmail.com>
2020-06-01 00:09:54 +0300

    SFML as static lib setup

--- Code/src/Game.cpp
+++ Code/src/Game.cpp
@@ -1,7 +1,20 @@
 #include <poorpch.h>
+#include <SFML/Window.hpp>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "hell to the champion" << std::endl;
+	sf::Window window(sf::VideoMode(800, 600), "15 Puzzle");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
 	return 0;
 }
